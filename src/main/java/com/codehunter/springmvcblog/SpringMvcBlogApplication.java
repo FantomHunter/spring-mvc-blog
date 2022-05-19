b74498b71d32f5863789d46665cf59276927fe77
hvhai.work <hunterx6323@gmail.com>
2022-05-19 08:47:00 +0700

    add previous/next for pagination

--- src/main/java/com/codehunter/springmvcblog/SpringMvcBlogApplication.java
+++ src/main/java/com/codehunter/springmvcblog/SpringMvcBlogApplication.java
@@ -3,6 +3,7 @@ package com.codehunter.springmvcblog;
 import com.codehunter.springmvcblog.entity.Post;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.IntStream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,13 +21,13 @@ public class SpringMvcBlogApplication {
   @Bean
   public CommandLineRunner demoData(PostRepository postRepository) {
     return args -> {
-      Post post1 = new Post();
-      post1.setTitle("title 1");
-      post1.setContent("content1".getBytes(StandardCharsets.UTF_8));
-      Post post2 = new Post();
-      post2.setTitle("title 2");
-      post2.setContent("content2".getBytes(StandardCharsets.UTF_8));
-      postRepository.saveAll(List.of(post1, post2));
+      IntStream.range(1,12).forEach(i -> {
+        Post post = new Post();
+        post.setTitle("title " + i);
+        String content = "Content " + i;
+        post.setContent(content.getBytes(StandardCharsets.UTF_8));
+        postRepository.save(post);
+      });
     };
   }
 }
